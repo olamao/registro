e5f725874813a387d31a8ca372de39adb0dc0fcb
mauricioolaya <olamao@gmail.com>
2025-04-28 05:43:57 -0500

    first

--- app/src/main/java/com/example/fendipetroleo/AgenteViewModel.kt
+++ app/src/main/java/com/example/fendipetroleo/AgenteViewModel.kt
@@ -4,27 +4,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fendipetroleo.data.agente.Agente
 import com.example.fendipetroleo.data.agente.AgenteRepository
+import com.example.fendipetroleo.data.volumen.Sale
+import com.example.fendipetroleo.data.volumen.SaleRepository
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.flow.Flow
 
-class AgenteViewModel(private val repository: AgenteRepository) : ViewModel() {
+class AgenteViewModel(private val repository1: AgenteRepository, private val repository2: SaleRepository) : ViewModel() {
 
-    val allAgents: Flow<List<Agente>> = repository.getAllAgents()
-    val allVolume: Flow<List<Venta>> = repository.getAllVenta()
+    val allAgents: Flow<List<Agente>> = repository1.getAllAgents()
+    val allVolume: Flow<List<Sale>> = repository2.getAllSales()
 
     fun addAgent(agente: Agente) {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                repository.addAgent(agente)
+                repository1.addAgent(agente)
             }
         }
     }
     fun getAgentById(id: Int) {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                repository.getAgentById(id)
+                repository1.getAgentById(id)
             }
         }
     }
